24565f66a71a23172dc999b9018e55478ee7c73d
simple-halwa-25 <113332135+simple-halwa-25@users.noreply.github.com>
2024-04-11 11:38:52 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let count=0; 
+document.getElementById("increase").onclick = function () {
+   count++; 
+  document.getElementById("count").textContent = count; 
+}
+document.getElementById("reset").onclick = function () {
+   count=0;
+  document.getElementById("count").textContent = count; 
+}
+document.getElementById("decrease").onclick = function () {
+   count--; 
+  document.getElementById("count").textContent = count; 
+}
